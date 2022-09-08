9e5a303c76475c70d2c54fee978be343a3934ca9
hikomecha <hikomecha987@gmail.com>
2022-09-08 06:50:44 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
  *   */
 int main(void)
 {
-	printf("With proper grammer, but the outcome is a piece of art\n ");
+	printf("With proper grammer, but the outcome is a piece of art,\n ");
 	return (0);
 }
