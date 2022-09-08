3c0980328ff9ca82709c3ecd0d93f00b42e23cd6
hikomecha <hikomecha987@gmail.com>
2022-09-08 06:54:01 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *   * main print the string in the printf function
- *
- *   * description using the main function
- *   *this program prints "with proper grammer, but the outcome is a piece of art"
- *
- *   * Returns when success
- *   */
+ * main print the string in the printf function
+
+ * description using the main function
+ * this program prints "with proper grammer, but the outcome is a piece of art"
+
+ * Returns when success
+ */
 int main(void)
 {
 	printf("With proper grammer, but the outcome is a piece of art,\n ");
