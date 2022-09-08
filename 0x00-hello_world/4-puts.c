cabe30a29cec35356eab5b0cd3d52d2192ad817d
hikomecha <hikomecha987@gmail.com>
2022-09-08 06:39:18 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,10 +8,12 @@
 
 int main(void)
 
+
 {
+                puts("\"Programming is like building a multilingual puzzle");            
+		
+		return (0);
+                                      
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
 
 }
