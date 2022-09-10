920ee4a535970afae5083e5cc0b6119e42e4a9f9
hikomecha <hikomecha987@gmail.com>
2022-09-10 08:23:06 -0700

    reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - lower case in reverse
+ *
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+char c = 'a';
+
+while (c => 'z')
+{
+	putchar(c);
+	c--;
+}
+return (0);
+}
