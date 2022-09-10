1d53e1845b8a3c355702d8ca6a9345f61730b0d4
hikomecha <hikomecha987@gmail.com>
2022-09-10 09:43:50 -0700

    base of 16 lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - base of 16
+ *
+ *
+ * Return: 0 when success
+ */
+int main(void)
+int i = '0';
+
+while (i <= '9')
+{
+	putchar(i);
+	i++;
+}
+i = 'a';
+while (i = 'f')
+{
+	putchar(i);
+	i++;
+}
+putchar('\n');
+return (0);
+}
