d762d297ea2371992f394682f3702e920b64b25a
hikomecha <hikomecha987@gmail.com>
2022-09-10 10:51:22 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - three digit combination
+ *
+ *
+ *
+ *Return: 0 when finished
+*/
+int main(void)
+{
+int i, j, n;
+for (i = '0'; i <= '9'; i++)
+{
+	for (j = '0'; j <= '9'; j++)
+	{
+		for (n = '0'; n <= '9'; n++)
+		{
+			if ((i < j) & (j < n) & (n <= '9'))
+			{
+				putchar(i);
+				putchar(j);
+				putchar(n);
+				
+				if ((i + j +n) < 168)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
