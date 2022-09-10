f94d79efa417e4aadaa64e58753b87b88d9e967b
hikomecha <hikomecha987@gmail.com>
2022-09-10 10:32:36 -0700

    100-combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <sdtio.h>
+/**
+ * main - different combination of two digit
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9', i++)
+{
+	for (j = '0', j <= '9', j++)
+	{
+		if ((i < j) & (j <= '9'))
+		{
+			putchar(i);
+			putchar(j);
+			if ((j < '9') | (i < '8'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
