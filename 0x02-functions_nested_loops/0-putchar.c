24f86d47d9d89580f359fbd8f26e59e1644d259e
hikomecha <hikomecha987@gmail.com>
2022-09-13 02:02:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - nested loop
+ *
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
