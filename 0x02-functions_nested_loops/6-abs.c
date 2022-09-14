72331eb52219c7ad7ad4a030acc080b5ea698786
hikomecha <hikomecha987@gmail.com>
2022-09-14 11:48:50 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function prints absolute
+ *@c: int
+ *Retun: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+		else
+			return (c * -1);
+}
+
