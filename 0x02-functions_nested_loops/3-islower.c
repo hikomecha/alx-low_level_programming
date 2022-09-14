2950e5d6051ec1265d2bca29edd0885ec017a2c3
hikomecha <hikomecha987@gmail.com>
2022-09-14 10:55:05 -0700

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks lower case
+ *
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
